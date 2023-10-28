e48131ef084f3ef5ec97b453bf758e9b4773e321
Drew <91377105+Drewski6@users.noreply.github.com>
2023-10-28 12:22:23 +0200

    Adding non interactive mode

--- testing_msh.py
+++ testing_msh.py
@@ -3,12 +3,12 @@ import pandas as pd
 import subprocess as sp
 import argparse
 import pdb
-import re
 
 ## SETTINGS ##
 testing_file = "unit_tests.xlsx"
 minishell_path = "./minishell"
-cwd = "/mnt/nfs/homes/dpentlan/Documents/ecole_42/minishell"
+# cwd = "/mnt/nfs/homes/nserve/Documents/42Cursus/minishell/minishell_00"
+cwd = "/mnt/nfs/homes/dpentlan/Documents/ecole_42/minishell/"
 bash_path = "bash"
 offset = 2  # offset is used so that the pandas row number
 # and the actual row number are the same.
@@ -44,7 +44,8 @@ ignore_list = [i - offset for i in
  282, #tests job control (&)
  47,48,49,347,704, #tests escaped character (\)
  61,62,63,284,348, #tests semicolon (;)
- 64,65,66,280,281,601,602,603,604,613,614,615,616,617,618,628, #tests bonus feature (parens)
+ 601,
+ # 64,65,66,280,281,602,603,604,613,614,615,616,617,618,628, #tests bonus feature (parens)
  72,73,149, #tests wildcards not in cwd (*/*)
  74,681, #tests . (source)
  76,367,444,445,446,472, #tests ~ (expands to value of HOME)
@@ -60,21 +61,23 @@ ignore_list = [i - offset for i in
  386, # tests history
  392,393,394,395,# tests pwd with options
  455,456,457,458, # idk spreadhseet says these are unhandled
- 58,59,60,558,575,576,577,578,579,580,581,582,583,584,585,586,585,586,587,588,589,590,591,592,593,594,594,595,596,597,598,599,600,605,606,607,608,609,610,611,612,619,620,621,622,623,624,625,626,627,629,630,631,632,633,634,635,636,637,638,639, #tests bonus feature (&&/||)
+ # 58,59,60,558,575,576,577,578,579,580,581,582,583,584,585,586,585,586,587,588,589,590,591,592,593,594,594,595,596,597,598,599,600,605,606,607,608,609,610,611,612,619,620,621,622,623,624,625,626,627,629,630,631,632,633,634,635,636,637,638,639, #tests bonus feature (&&/||)
 250, 251,   # export with options.
 355,        # unset with options
-761,        # super heredoc
 726,        # race condition
 641,        # too hard to test lol
 463,        # $SHLVL 
 423,424,    # //
-146,126,122        # special variables in bash
+146,126,122,        # special variables in bash
+224,231,232,236,244,245,246,247,263,266,267,268,271,272,273,274,275,276,277,278,283,286,287,292,298,316,318,319,331,332,384, #env
+745, 746, 747, 748, 749, 750, 751, 752, 753, 754, 755, 756, 757, 758, 759, 760, 761,        # super heredoc
 ]]
 
 valgrind_ignore_list = [i - offset for i in 
 [
 459,
-537, 538 # contain the command ifconfig which apparently leaks
+537, 538, # contain the command ifconfig which apparently leaks
+735, 601,#not test
 ]]
 #still included
 # 71,129,140,141,142,143,144,145,146,147,148,149,447,448,449,764,765,766,767,768,769, #tests bonus feature (wildcard)
@@ -104,35 +107,52 @@ def print_results(tests_conducted: int, tests_succeeded: int):
     print("\n*******************\n")
     print(f"Total tests: {tests_conducted}")
     print(f"Successes  : {tests_succeeded}")
-    print(f"Percentage : {tests_succeeded / tests_conducted * 100:.2f}%")
+    if (tests_conducted != 0):
+        print(f"Percentage : {tests_succeeded / tests_conducted * 100:.2f}%")
 
 
-
-
-def single_test(test, command):
-    result = sp.run(
-        f"echo '{test}' | {command}",
+def run_test(test: str):
+    if (args.non_interactive):
+        test = str(test).replace("\n", " && ")
+        test = test[:-4]
+    stdoutdiff = False
+    stderrdiff = False
+    returndiff = False
+    if (args.non_interactive):
+        input = f"{minishell_path} {test}",
+    else:
+        input = f"echo '{test}' | {minishell_path}",
+    result_msh = sp.run(
+        f"{input}",
         shell=True,
         stdout=sp.PIPE,
         stderr=sp.PIPE,
         text=True,
-        cwd=cwd,
+        executable="bash",
+        cwd=cwd
     )
-    result.stderr = re.sub(r"bash: line \d*:", "shell:", result.stderr)
-    result.stderr = re.sub(r"^minishell:", "shell:", result.stderr)
-    return result
-
-
-def run_test(test: str):
-    result_msh = single_test(test, minishell_path)
-    result_bash = single_test(test, bash_path)
-    stdoutdiff = args.no_stdout and (result_msh.stdout != result_bash.stdout)
-    stderrdiff = args.no_stderr and (result_msh.stderr != result_bash.stderr)
-    returndiff = args.no_return and (result_msh.returncode != result_bash.returncode)
+    result_bash = sp.run(
+        f"echo '{test}' | {bash_path}",
+        shell=True,
+        stdout=sp.PIPE,
+        stderr=sp.PIPE,
+        text=True,
+        executable="bash",
+        cwd=cwd
+    )
+    if args.no_stdout:
+        if result_msh.stdout != result_bash.stdout:
+            stdoutdiff = True
+    if args.no_stderr:
+        if result_msh.stderr != result_bash.stderr:
+            stderrdiff = True
+    if args.no_return:
+        if result_msh.returncode != result_bash.returncode:
+            returndiff = True
     print(f"TEST {line + offset}", end="")
     if not stdoutdiff and not stderrdiff and not returndiff:
         print(" OK!")
-        return True
+        return 0
     else:
         print(f" Error!\n\n{test}")
         print("    RESULTS MINISHELL")
@@ -146,21 +166,30 @@ def run_test(test: str):
         print(
             f"{line_form(returndiff)}return: {tab_form(str(result_bash.returncode))}\n"
         )
-    return False
+    return 1
 
 
-def run_test_valgrind(test: str, line: int):
-    result_msh = single_test(test, f"valgrind --leak-check=full --track-origins=yes --track-fds=yes --trace-children=yes --show-leak-kinds=all --suppressions=/mnt/nfs/homes/dpentlan/Documents/ecole_42/minishell/supp.supp {minishell_path}")
-    print(f"\nTEST {line + offset}", end="")
-    print(f"\n\n{test}")
-    print(result_msh.stderr)
+def run_test_valgrind(test):
+    result_msh = sp.run(
+        f"echo '{test}' | valgrind --leak-check=full --track-origins=yes --track-fds=yes --trace-children=yes --show-leak-kinds=all --suppressions={cwd}/supp.supp {minishell_path}",
+        shell=True,
+        stdout=sp.PIPE,
+        stderr=sp.PIPE,
+        text=True,
+        executable="bash",
+        cwd=cwd
+    )
+    # if "blocks are indirectly lost" in result_msh.stderr:
+    #     print(f"{result_msh.stderr}")
+    #     return 1
     for line in result_msh.stderr.split("\n"):
         if ("definitely lost" in line or "indirectly lost" in line or "possibly lost" in line or "still reachable" in line or "Open file descriptor " in line) and " 0 " not in line:
             if args.valgrind_stderr:
+                print(result_msh.stderr)
                 return False
-    print(f"RETURN: {result_msh.returncode}")
     print("*", end="", flush=True)
-    return True
+    return 0
+
 
 
 def get_test_input(line: int):
@@ -170,23 +199,6 @@ def get_test_input(line: int):
     test = "\n".join(tests)
     test += "\n"
     return test
-
-
-def test_select(line):
-    test = get_test_input(line)
-    if args.valgrind or args.valgrind_stderr:
-        if run_test_valgrind(test, line):
-            cleanup_test_files(files_to_delete)
-            return True
-        else:
-            print(f"test {line + 2} Failed valgrind")
-            cleanup_test_files(files_to_delete)
-            return False
-    elif run_test(test):
-        cleanup_test_files(files_to_delete)
-        return True
-    cleanup_test_files(files_to_delete)
-    return False
 
 
 parser = argparse.ArgumentParser(description="A tester for minishell.")
@@ -223,6 +235,12 @@ parser.add_argument(
     help="test will skip comparisons to return."
 )
 parser.add_argument(
+    "--non_interactive",
+    "-ni",
+    action="store_true",
+    help="test will run tests in non-interactive mode."
+)
+parser.add_argument(
     "--valgrind",
     "-vg",
     action="store_true",
@@ -238,13 +256,22 @@ args = parser.parse_args()
 df = pd.read_excel(testing_file)
 test_col = 1
 bash_col = 7
+# pdb.set_trace()
 if args.arg1 >= 26 and args.arg1 <= 769 and args.arg2 >= 26 and args.arg2 <= 769:
     start = args.arg1 - offset
     end = args.arg2 - offset + 1
 if args.arg1 != -1 and args.arg2 == -1:
     line = args.arg1 - offset
-    tests_conducted += 1
-    tests_succeeded += test_select(line)
+    tests_conducted = tests_conducted + 1
+    test = get_test_input(line)
+    if args.valgrind or args.valgrind_stderr:
+        if not run_test_valgrind(test):
+            tests_succeeded = tests_succeeded + 1
+        else:
+            print(f"test {line + 2} Failed valgrind")
+    elif not run_test(test):
+        tests_succeeded = tests_succeeded + 1
+    cleanup_test_files(files_to_delete)
 else:
     for line in range(start, end):
         if args.valgrind or args.valgrind_stderr:
@@ -253,8 +280,15 @@ else:
         else:
             if line in ignore_list:
                 continue
-        tests_conducted += 1
-        tests_succeeded += test_select(line)
-
+        test = get_test_input(line)
+        tests_conducted = tests_conducted + 1
+        if args.valgrind or args.valgrind_stderr:
+            if not run_test_valgrind(test):
+                tests_succeeded = tests_succeeded + 1
+            else:
+                print(f"test {line + 2} Failed valgrind")
+        elif not run_test(test):
+            tests_succeeded = tests_succeeded + 1
+        cleanup_test_files(files_to_delete)
 
 print_results(tests_conducted, tests_succeeded)
